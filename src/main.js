3cfaa7b1d717152fd0f4aed89f0250726159f0f8
candyapplecorn@gmail.com <candyapplecorn@gmail.com>
2018-11-11 23:31:46 -0800

    trying to get heroku working

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import VueCarousel from 'vue-carousel'
-
-Vue.use(VueCarousel)
 
 Vue.config.productionTip = false
 
