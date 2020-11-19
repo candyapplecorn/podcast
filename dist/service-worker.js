3f4e66e870cf236e874ab2ac5af697f34425f6a0
Joseph Burger <joseph@airkit.com>
2020-11-19 01:34:35 -0800

    created a directoy called dist, removed /dist from .gitignore, and then ran npx vue-cli-service build to produce a static bundle

---
+++ dist/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/podcast/precache-manifest.6bd7488b7a0a01e06773ff172469d6d5.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "podcast"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
