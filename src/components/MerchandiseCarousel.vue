ce8cf99c047ea96f656546b424c6f17062de4d26
Joseph Burger <joseph@airkit.com>
2020-12-06 14:20:03 -0800

    lol bash for thing in `ls -lth ~/Downloads | egrep -e png -e jpg | head -n 4 | awk '{ print $9 }'`; do cp ~/Downloads/${thing} src/assets/hanging_from_the_rafters_merch_${thing}; done

---
+++ src/components/MerchandiseCarousel.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <VueSlickCarousel :arrows="true" :dots="true">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </VueSlickCarousel>
+  </div>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+export default {
+  name: "MerchandiseCarousel",
+  components: { VueSlickCarousel },
+}
+</script>
+
+<style scoped>
+
+</style>
